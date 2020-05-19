e47fc6c23fecdc841a1bb22ccca457aefc855b5b
Rishab <rishabravikumar@yahoo.com>
2020-05-19 00:23:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 
 const port = process.env.PORT || 8080;
 
+app.get('/', function(req, res) {
+    res.send('<h1>node-simple-scraper</h1>')
+});
 
 app.listen(port, () => {
     console.log(`Server running at port `+port);
